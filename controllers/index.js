d94bc57f925c434dae11ec3f1a01a6db96cfa0c5
Mpergolatti <Mpergolatti@gmail.com>
2022-10-31 20:47:09 -0400

    added index info

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const activeRoutes = require('./active.routes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/active', activeRoutes);
 
 module.exports = router;
