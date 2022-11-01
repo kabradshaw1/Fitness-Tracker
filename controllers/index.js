3e199bbe1925a06ee75bc7c8557e9f70a984cce8
Ed Beard <ed.r.beard@gmail.com>
2022-11-01 14:12:40 -0400

    update index files

--- controllers/index.js
+++ controllers/index.js
@@ -2,20 +2,16 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-<<<<<<< HEAD
-const stepsRoutes = require('./steps-routes.js');
-=======
 const activeRoutes = require('./active-routes')
 const heartRoutes = require('./heart-routes.js');
->>>>>>> 899a1e201e408d608f69654790bb83d907831fa4
+const stepsRoutes = require('./steps-routes.js');
 
 router.use('/', homeRoutes);
-<<<<<<< HEAD
-router.use('/steps', stepsRoutes);
-=======
 router.use('/active', activeRoutes);
 router.use('/heart', heartRoutes);
 router.use('/api', apiRoutes);
->>>>>>> 899a1e201e408d608f69654790bb83d907831fa4
+router.use('/', homeRoutes);
+router.use('/steps', stepsRoutes);
+
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,10 +1,6 @@
 const Heart = require('./Heart');
 const User = require('./User');
-<<<<<<< HEAD
-const Steps = require('./Steps');
-=======
-const Active = require('./Active');
->>>>>>> 899a1e201e408d608f69654790bb83d907831fa4
+const Steps = require('./Steps')
 
 User.hasMany(Heart, {
   foreignKey: 'user_id'
@@ -19,14 +15,6 @@ Heart.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-<<<<<<< HEAD
-Steps.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-})
-
-module.exports = { Heart, User, Steps };
-=======
 User.hasMany(Active, {
   foreignKey: 'user_id'
 });
@@ -36,5 +24,4 @@ Active.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-module.exports = { Heart, User, Active };
->>>>>>> 899a1e201e408d608f69654790bb83d907831fa4
+module.exports = { Heart, User, Steps };
