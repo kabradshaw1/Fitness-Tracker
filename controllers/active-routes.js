78214ff5dd016ad7208b318f4c7347ebe8cf56dc
Mpergolatti <Mpergolatti@gmail.com>
2022-10-31 21:04:59 -0400

    Added active page route

---
+++ controllers/active-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Active, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+  console.log('======================');
+  Active.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'qty',
+      'date',
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbactiveData => {
+    const active_rate = dbactiveData.map(active => active.get({ plain: true }));
+    res.render('active', { active_rate, loggedIn: true });
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const activeRoutes = require('./active.routes')
+const activeRoutes = require('./active-routes')
 const heartRoutes = require('./heart-routes.js');
 
 router.use('/', homeRoutes);
