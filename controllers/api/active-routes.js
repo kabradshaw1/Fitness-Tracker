9eca633c8c64ad3778f9973b82e7125975b2dafc
Mpergolatti <Mpergolatti@gmail.com>
2022-11-01 16:14:19 -0400

    added seeds user_id

--- controllers/api/active-routes.js
+++ controllers/api/active-routes.js
@@ -2,6 +2,52 @@ const router = require('express').Router();
 const { Active, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', (req, res) => {
+  Active.findAll({
+    attributes: [
+      'id',
+      'qty',
+      'date',
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbActiveData => res.json(dbActiveData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.get('/:id', (req, res) => {
+  Active.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'date',
+      'qty'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbActiveData => res.json(dbActiveData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post('/', withAuth, (req, res) => {
   Active.create({
     user_id: req.body.user_id,
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const heartRoutes = require('./heart-routes');
 const userRoutes = require('./user-routes')
+const activeRoutes = require('./active-routes')
 
 router.use('/heart', heartRoutes);
 router.use('/user', userRoutes)
+router.use('/active', activeRoutes)
 
 module.exports = router;
