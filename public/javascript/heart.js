905e87874aef0a38452b0d500dfbaa1bc30ec537
kabradshaw1 <itllbok82@gmail.com>
2022-11-02 09:37:20 -0400

    working on public/heart.js

--- public/javascript/heart.js
+++ public/javascript/heart.js
@@ -1,4 +1,3 @@
-let data = []
 
 async function heartFormHandler(event) {
   event.preventDefault();
@@ -31,9 +30,8 @@ getHeart = () => {
   fetch('/api/heart')
     .then(response => {
       if(response.ok) {
-        response.json().then(function(data1) {
-          data = data1;
-          
+        response.json().then(function(data) {
+          displayGraph(data)
         })
       } else {
         alert('Error: Item Not Found');
@@ -56,34 +54,37 @@ getHeart = () => {
 // d3.json('http://localhost:3001/api/heart', function (d) {
 //   console.log(d)
 // })
-const width = 800;
-const height = 400;
-const margin = { top: 50, bottom: 50, left: 50, right: 50};
 
-const svg = d3.select('#d3-container')
-  .append('svg')
-  .attr('height', height - margin.top - margin.bottom)
-  .attr('width', width - margin.left - margin.right)
-  .attr('viewBox', [0, 0, width, height]);
+const displayGraph = (data) => {
+  const width = 800;
+  const height = 400;
+  const margin = { top: 50, bottom: 50, left: 50, right: 50};
 
-const x = d3.scaleBand()
-  .domain(d3.range(data.length))
-  .range([margin.left, width - margin.right])
-  .padding(0.1);
+  const svg = d3.select('#d3-container')
+    .append('svg')
+    .attr('height', height - margin.top - margin.bottom)
+    .attr('width', width - margin.left - margin.right)
+    .attr('viewBox', [0, 0, width, height]);
 
-const y = d3.scaleLinear()
-  .domain([0, 150])
-  .range([height - margin.bottom, margin.top])
-svg
-  .append('g')
-  .attr('fill', 'royalblue')
-  .selectAll('rect')
-  .join('rect')
-    .attr('x', (d, i) => x(i))
-    .attr('y', (d) => y(d.parseInt(max)))
-    .attr('height', d => y(0) - y(d.parseInt(max)))
-    .attr('width', x.bandwidth())
+  const x = d3.scaleBand()
+    .domain(d3.range(data.length))
+    .range([margin.left, width - margin.right])
+    .padding(0.1);
 
-svg.node();
+  const y = d3.scaleLinear()
+    .domain([0, 150])
+    .range([height - margin.bottom, margin.top])
+  svg
+    .append('g')
+    .attr('fill', 'royalblue')
+    .selectAll('rect')
+    .join('rect')
+      .attr('x', (d, i) => x(i))
+      .attr('y', (d) => y(d.parseInt(max)))
+      .attr('height', d => y(0) - y(d.parseInt(max)))
+      .attr('width', x.bandwidth())
+
+  svg.node();
+}
 document.querySelector('#heart-form').addEventListener('submit', heartFormHandler);
 getHeart()
