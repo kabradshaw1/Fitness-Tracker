165bef9ceb4321fa0b4c178bd7886f16c471e9f2
dewittd14 <deanna.dewitt14@gmail.com>
2022-11-02 19:12:22 -0400

    added distance

---
+++ controllers/api/distance-routes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { Distance, User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+  Distance.findAll({
+    attributes: [
+      'id',
+      'qty',
+      'date',
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbDistanceData => res.json(dbDistanceData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.get('/:id', (req, res) => {
+  Distance.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'qty',
+      'date'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbDistanceData => res.json(dbDistanceData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+  Distance.create({
+    qty: req.body.qty,
+    user_id: req.body.user_id,
+    date: req.body.date
+  })
+  .then(dbDistanceData => res.json(dbDistanceData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  Distance.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbDistanceData => {
+    if (!dbDistanceData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbDistanceData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
---
+++ models/Distance.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Distance extends Model {}
+
+Distance.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true
+      }
+    },
+    qty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInteger: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'distance',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'distance'
+  }
+)
+
+module.exports = Distance;
