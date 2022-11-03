b053434ee57aaa8585621c147abecd89d6442d87
kabradshaw1 <itllbok82@gmail.com>
2022-11-03 13:31:08 -0400

    dates added to graph, but not formatted properly yet

--- public/javascript/heart.js
+++ public/javascript/heart.js
@@ -3,7 +3,6 @@ async function heartFormHandler(event) {
 
   const max = document.querySelector('#max-input').value.trim();
   const date = new Date();
-  console.log(max, date)
 
   const response = await fetch('/api/heart', {
     method: 'POST',
@@ -33,22 +32,34 @@ const displayGraph = (data) => {
     .domain(data.map(dataPoint => dataPoint.id))
     .rangeRound([0, 800]).padding(0.1);  
   const yScale = d3
-  .scaleLinear()
-  // you will need to change the upper end of the domain, here it says 180, to be something slightly out of the range of your data.
-  // so, for example, if your highest data point is 1000, you want to go 1100.
-  .domain([0, 180])
-  .range([400, 0]);
+    .scaleLinear()
+    // you will need to change the upper end of the domain, here it says 180, to be something slightly out of the range of your data.
+    // so, for example, if your highest data point is 1000, you want to go 1100.
+    .domain([0, 180])
+    .range([400, 0]);
 
-  const chart = d3.select('#d3-container')
+  const chart = d3.select('#d3-container');
+  
+  const bar = chart
     .selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('rect')
+        .classed('bar', true)
+        .attr('width', xScale.bandwidth())
+        // you will need to change max to what ever variable you are trying to graph on this line and down in the y attribute
+        .attr('height', d =>400 - yScale(d.max))
+        .attr('x', d => xScale(d.id))
+        .attr('y', d => yScale(d.max));
+  const dates = chart
+    .selectAll('text')
       .data(data)
       .enter()
-      .append('rect')
-      .classed('bar', true)
-      .attr('width', xScale.bandwidth())
-      // you will need to change max to what ever variable you are trying to graph on this line and down in the y attribute
-      .attr('height', d =>400 - yScale(d.max))
+      .append('text')
+      // .classed('chart-dates', true)
+      .text(d => d.date)
       .attr('x', d => xScale(d.id))
-      .attr('y', d => yScale(d.max));
+      .attr('y', 400);
+
 };
 
