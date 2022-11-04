31a57f8129087ea95aed16e176e0f5395b2a4d3e
kabradshaw1 <itllbok82@gmail.com>
2022-11-04 11:25:40 -0400

    graph rendering

--- public/javascript/steps.js
+++ public/javascript/steps.js
@@ -24,31 +24,40 @@ async function stepsFormHandler(event) {
   
   document.querySelector('#steps-form').addEventListener('submit', stepsFormHandler);
   
-  d3.json("/api/steps").then(data => displayGraph(data));
+d3.json("/api/steps").then(data => displayGraph(data));
+
+const displayGraph = (data) => {
   
-  const displayGraph = (data) => {
-    const xScale = d3
-      .scaleBand()
-      .domain(data.map(dataPoint => dataPoint.id))
-      .rangeRound([0, 800]).padding(0.1);  
-    const yScale = d3
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map(dataPoint => dataPoint.id))
+    .rangeRound([0, 800]).padding(0.1);  
+  const yScale = d3
     .scaleLinear()
+    // you will need to change the upper end of the domain, here it says 180, to be something slightly out of the range of your data.
+    // so, for example, if your highest data point is 1000, you want to go 1100.
     .domain([0, 24000])
     .range([400, 0]);
+  const formatted = data.map(d => {
+    const date = new Date(d.date);
+    const newDate = date.getDate();
+    return { ...d, date:newDate }
+  });
+  console.log(formatted)
+  const chart = d3.select('#d3-container');
   
-    const chart = d3.select('#stepsd3-container')
-  
-    const bars = chart
-      .selectAll('.bar')
-        .data(data)
-        .enter()
-        .append('rect')
-        .classed('bar', true)
-        .attr('width', xScale.bandwidth())
-        .attr('height', d =>400 - yScale(d.qty))
-        .attr('x', d => xScale(d.id))
-        .attr('y', d => yScale(d.qty));
-    const dates = chart
+  const bar = chart
+    .selectAll('.bar')
+      .data(formatted)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('width', xScale.bandwidth())
+      // you will need to change max to what ever variable you are trying to graph on this line and down in the y attribute
+      .attr('height', d =>400 - yScale(d.qty))
+      .attr('x', d => xScale(d.id))
+      .attr('y', d => yScale(d.qty));
+  const dates = chart
     .selectAll('text')
       .data(formatted)
       .enter()
@@ -57,4 +66,3 @@ async function stepsFormHandler(event) {
       .attr('x', d => xScale(d.id))
       .attr('y', 395);
 };
- 
