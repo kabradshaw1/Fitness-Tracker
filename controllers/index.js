ef8cb2fde3df028d8ef58fddbc070400615b35f4
dewittd14 <deanna.dewitt14@gmail.com>
2022-11-07 17:39:59 -0500

    updated distance to have correct data

--- controllers/index.js
+++ controllers/index.js
@@ -2,15 +2,18 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const activeRoutes = require('./active-routes')
+const activeRoutes = require('./active-routes');
 const heartRoutes = require('./heart-routes.js');
 const stepsRoutes = require('./steps-routes.js');
+const distanceRoutes = require('./distance-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/active', activeRoutes);
 router.use('/heart', heartRoutes);
 router.use('/api', apiRoutes);
 router.use('/steps', stepsRoutes);
+router.use('/distance', distanceRoutes);
+
 
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const seedHeart = require('./heart-seeds');
 const seedUsers = require('./user-seeds');
 const seedDistance = require('./distance-seeds');
 const seedSteps = require('./steps-seeds');
+const seedActive = require('./active-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
@@ -11,8 +12,11 @@ const seedAll = async () => {
   console.log('--------------');
   await seedHeart();
   console.log('--------------');
-  await seedDistance();
+  //await seedActive();
+  //console.log('--------------');
   await seedSteps();
+  console.log('--------------');
+  await seedDistance();
   console.log('--------------');
   
   process.exit(0);
