cb5fc3e5c4d9327214abd700b390d8f6a0aa64f1
Mpergolatti <Mpergolatti@gmail.com>
2022-11-07 20:26:41 -0500

    fixed merge conflicts

--- seeds/index.js
+++ seeds/index.js
@@ -9,18 +9,20 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
+
   await seedUsers(); 
   console.log('--------------');
+
   await seedHeart();
   console.log('--------------');
 
   await seedDistance();
   console.log('--------------');
+
   await seedSteps();
   console.log('--------------');
+  
   await seedActive();
-  console.log('--------------');
-  await seedDistance();
   console.log('--------------');
   
   process.exit(0);
