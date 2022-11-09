29558aacca365ed87d7834bed664f0d4c32fff9c
kabradshaw1 <itllbok82@gmail.com>
2022-11-09 15:22:38 -0500

    fixed inputs to work for all graphs

--- models/Heart.js
+++ models/Heart.js
@@ -22,13 +22,6 @@ Heart.init(
         isDate: true
       }
     },
-    // chart: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    //   validate: {
-    //     isString: true
-    //   }
-    // },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
